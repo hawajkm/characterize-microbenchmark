3d8e2e4fe2817e7f8ffa57f9797091f355f98066
hawajkm <hawajkm@gmail.com>
2023-11-13 09:27:23 -0500

    [impl] set GCC opt level per code

--- src/impl/naive.c
+++ src/impl/naive.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 
-// Naive Implementation
+/* Naive Implementation */
+#pragma GCC push_options
+#pragma GCC optimize ("O1")
 void* impl_scalar_naive(void* dest, const void* src, size_t size)
 {
 }
+#pragma GCC pop_options
--- src/impl/opt.c
+++ src/impl/opt.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 
-// Alternative Implementation
+/* Alternative Implementation */
+#pragma GCC push_options
+#pragma GCC optimize ("O1")
 void* impl_scalar_opt(void* dest, const void* src, size_t size)
 {
 }
+#pragma GCC pop_options
