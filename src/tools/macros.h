181678e747d174afd9a0aee8c830c78669d918ec
hawajkm <hawajkm@gmail.com>
2023-11-13 09:29:10 -0500

    [macros] add compiler fence

--- src/tools/macros.h
+++ src/tools/macros.h
@@ -1,5 +1,12 @@
-// macros.h
-//
-// File containing all required macros for characterizing-microbenchmark
+/* macros.h
+ *
+ * Author: Khalid Al-Hawaj
+ * Date  : 12 Nov. 2023
+ *
+ * File containing all required macros for characterizing-microbenchmark */
 
+/* General */
+#define __COMPILER_FENCE_ __asm__ __volatile__ ("" : : : "memory");
+
+/* Specific */
 #define __PRINT_MATCH(x) (x ? "   MATCH" : "NO MATCH")
