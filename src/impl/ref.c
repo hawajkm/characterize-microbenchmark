ef051feb0aa98e4da8d596188bf7938a3283656a
hawajkm <hawajkm@gmail.com>
2023-11-16 21:33:38 -0500

    [ref] add ref function

---
+++ src/impl/ref.c
@@ -0,0 +1,19 @@
+/* ref.c
+ *
+ * Author: 
+ * Date  :
+ *
+ *  Description
+ */
+
+/* Standard C includes */
+#include <stdlib.h>
+
+/* Include all required headers */
+#include "common/macros.h"
+#include "common/types.h"
+
+/* Reference Implementation */
+void* impl_ref(void* dest, const void* src, size_t size)
+{
+}
