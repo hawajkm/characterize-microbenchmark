c9c0b56298e4ba82d07b7eef76f9d720e3c6a6a4
hawajkm <hawajkm@gmail.com>
2023-12-22 13:36:38 -0500

    [impl][vec] add support for vectorized implementations

---
+++ src/impl/vec.c
@@ -0,0 +1,22 @@
+/* vec.c
+ *
+ * Author:
+ * Date  :
+ *
+ *  Description
+ */
+
+/* Standard C includes */
+#include <stdlib.h>
+
+/* Include all required headers */
+#include "common/macros.h"
+#include "common/types.h"
+
+/* Alternative Implementation */
+#pragma GCC push_options
+#pragma GCC optimize ("O1")
+void* impl_vector(void* args)
+{
+}
+#pragma GCC pop_options
