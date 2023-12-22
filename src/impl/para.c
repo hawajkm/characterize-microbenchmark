ae29676436e33f69d1e6f461f99452ee69e5b935
hawajkm <hawajkm@gmail.com>
2023-12-22 13:39:57 -0500

    [impl][para] add support for parallelized implementations

---
+++ src/impl/para.c
@@ -0,0 +1,22 @@
+/* para.c
+ *
+ * Author:
+ * Date  :
+ *
+ *  Description
+ */
+
+/* Standard C includes */
+#include <stdlib.h>
+
+/* Include all required headers */
+#include "common/macros.h"
+#include "common/types.h"
+
+/* Alternative Implementation */
+#pragma GCC push_options
+#pragma GCC optimize ("O1")
+void* impl_parallel(void* args)
+{
+}
+#pragma GCC pop_options
