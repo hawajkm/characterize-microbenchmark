87b1d85ce1a951733cdad2b5a13e35352c5933ea
hawajkm <hawajkm@gmail.com>
2023-12-31 08:31:58 -0500

    [common][mach_pthread_compatibility] remove printfs inside call

--- src/common/mach_pthread_compatibility.h
+++ src/common/mach_pthread_compatibility.h
@@ -49,7 +49,7 @@ int pthread_setaffinity_np(pthread_t thread, size_t cpu_size,
   for (core = 0; core < 8 * cpu_size; core++) {
     if (CPU_ISSET(core, cpu_set)) break;
   }
-  printf("binding to core %d\n", core);
+  //printf("binding to core %d\n", core);
   thread_affinity_policy_data_t policy = { core };
   mach_thread = pthread_mach_thread_np(thread);
   thread_policy_set(mach_thread, THREAD_AFFINITY_POLICY,
