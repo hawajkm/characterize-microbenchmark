f445d7c000ea511898aa70ee570b37c4c9471fbd
hawajkm <hawajkm@gmail.com>
2023-12-30 23:03:47 -0500

    [vvadd][para] add includes when Darwin

--- src/vvadd/impl/para.c
+++ src/vvadd/impl/para.c
@@ -18,6 +18,11 @@
 #include "common/macros.h"
 #include "common/types.h"
 
+/* If we are on Darwin, include the compatibility header */
+#if defined(__APPLE__)
+#include "common/mach_pthread_compatibility.h"
+#endif
+
 /* Include application-specific headers */
 #include "include/types.h"
 
